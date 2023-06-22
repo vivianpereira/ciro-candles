34a5706ac24a2395dd9bd879400012edb83fb8e1
Douglas Marques <dmarques@atlassian.com>
2023-06-22 17:38:15 +1000

    Adding routes

--- ciro_candles/views.py
+++ ciro_candles/views.py
@@ -1,27 +1,27 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 
 bp = Blueprint("main", __name__)
 
+@bp.route("/home/")
+def home():
+    return render_template("index.html")
 
 @bp.route("/")
 def index():
-    return render_template("index.html")
+    return redirect("/home")
 
+@bp.route("/category/<name>")
+def category(name):
+    return render_template("category.html")
 
-@bp.route("/secret/")
-def secret():
-    return '<h1 style="color:yellow;">You found the secret page</h1>'
+@bp.route("/detail/")
+def detail():
+    return render_template("detail.html")
 
+@bp.route("/basket/")
+def basket():
+    return render_template("basket.html")
 
 @bp.route("/checkout/", methods=["POST", "GET"])
 def checkout():
-    # print form paramenters sent using GET
-    print(
-        "Firstname: {}\nSurname: {}\nPhone: {}".format(
-            request.values.get("firstname"),
-            request.values.get("surname"),
-            request.values.get("phone"),
-        )
-    )
-
     return render_template("checkout.html")
